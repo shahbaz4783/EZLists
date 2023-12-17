2a0ca45ae8d40b026d64c2ad73a54adac1230221
shahbaz4783 <shahbaz4783@gmail.com>
2023-12-17 21:52:58 +0530

    styling update

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	TouchableOpacity,
+} from 'react-native';
 
 export default function App() {
 	const [enteredText, setEnteredText] = useState('');
@@ -23,11 +29,15 @@ export default function App() {
 					placeholder='Enter Your Goals'
 					onChangeText={inputHandler}
 				/>
-				<Button onPress={buttonHandler} title='Add' />
+				<TouchableOpacity onPress={buttonHandler} style={styles.customButton}>
+					<Text style={styles.buttonText}>Add</Text>
+				</TouchableOpacity>
 			</View>
-			<View>
+			<View style={styles.textContainer}>
 				{allText.map((text, index) => (
-					<Text key={index}>{text}</Text>
+					<Text style={styles.textContent} key={index}>
+						{index + 1} {text}
+					</Text>
 				))}
 			</View>
 		</View>
@@ -38,14 +48,16 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		margin: 24,
-		borderWidth: 2,
-		borderColor: 'blue',
+		// borderWidth: 2,
+		// borderColor: 'blue',
 	},
+
 	inputContainer: {
 		flexDirection: 'row',
 		justifyContent: 'space-between',
 		alignItems: 'center',
 		gap: 24,
+		flex: 2,
 	},
 	input: {
 		flex: 1,
@@ -54,11 +66,26 @@ const styles = StyleSheet.create({
 		padding: 12,
 		fontSize: 18,
 	},
+	customButton: {
+		backgroundColor: 'green',
+		padding: 10,
+		borderRadius: 5,
+	},
+	buttonText: {
+		color: 'white',
+		padding: 8,
+	},
 
+	textContainer: {
+		flex: 5,
+		overflow: 'scroll',
+		gap: 10,
+	},
 	textContent: {
-		borderWidth: 2,
-		borderColor: 'blue',
+		backgroundColor: 'blue',
+		color: 'white',
 		padding: 16,
-		margin: 16,
+		borderRadius: 6,
+		fontSize: 18,
 	},
 });
