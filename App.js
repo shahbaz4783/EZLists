435600559013bca34455d89d8b68eca8ff17d3d0
shahbaz4783 <shahbaz4783@gmail.com>
2023-12-17 21:07:20 +0530

    render texts on screen

--- App.js
+++ App.js
@@ -1,35 +1,59 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+	const [enteredText, setEnteredText] = useState('');
+	const [allText, setAllText] = useState([]);
+
+	const inputHandler = (e) => {
+		setEnteredText(e);
+	};
+
+	const buttonHandler = () => {
+		setAllText((currTexts) => {
+			return [...currTexts, enteredText];
+		});
+	};
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.inputContainer}>
-				<TextInput style={styles.input} placeholder='Enter Your Goals' />
-				<Button title='Add' />
+				<TextInput
+					style={styles.input}
+					placeholder='Enter Your Goals'
+					onChangeText={inputHandler}
+				/>
+				<Button onPress={buttonHandler} title='Add' />
 			</View>
-      <View>
-			<Text style={styles.textContent}>List of goals...</Text>
-      </View>
+			<View>
+				{allText.map((text, index) => (
+					<Text key={index}>{text}</Text>
+				))}
+			</View>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
+		flex: 1,
 		margin: 24,
 		borderWidth: 2,
 		borderColor: 'blue',
 	},
 	inputContainer: {
 		flexDirection: 'row',
-    justifyContent: 'space-between',
-    gap: 10,
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		gap: 24,
 	},
-  input: {
-    flex: 1,
-    borderWidth: 2,
-    borderColor: 'grey'
-  },
+	input: {
+		flex: 1,
+		borderWidth: 2,
+		borderColor: 'grey',
+		padding: 12,
+		fontSize: 18,
+	},
 
 	textContent: {
 		borderWidth: 2,
